3e0b066a1234038c73996e00cc35ee1f00028b38
Nikhil Prasanna <nikprasanna@gmail.com>
2022-01-08 11:39:11 -0500

    Removed axios

--- wombeats-client/src/App.js
+++ wombeats-client/src/App.js
@@ -4,24 +4,11 @@ import * as React from 'react';
 import ReactDOM from 'react-dom';
 import SearchInputForm from './SearchInputForm.js';
 import SearchResultTable from './SearchResultTable.js';
-import axios from 'axios';
 
 function App() {
-  const submitHandler = (e) => {
-        e.preventDefault();
-        axios.post("http://localhost:8083/createmodule", state)
-        .then( response => {
-            console.log(response)
-        })
-        .catch( error => {
-            console.log(error)
-        })
-        console.log(moduletype);
-    }
-
   return (
             <div>
-                <SearchInputForm onChange={handleChange}/>
+                <SearchInputForm />
                 <SearchResultTable />
             </div>
          );
