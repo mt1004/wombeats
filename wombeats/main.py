fcfd909a09dfe77fc058bc75fc0511fe3dbd9833
Nikhil Prasanna <nikprasanna@gmail.com>
2022-02-21 11:31:22 -0500

    Clean up main

--- wombeats/main.py
+++ wombeats/main.py
@@ -1,5 +1,4 @@
 import json
-import time
 from decimal import Decimal
 
 import spotipy
@@ -9,17 +8,13 @@ from spotipy.oauth2 import SpotifyClientCredentials
 from wombeats.api_access import SpotifyAPIAccess
 from wombeats.constants import CLI_ID, CLI_SEC, REDIRECT_URI, SCOPE
 from wombeats.models import SearchQuery
+from wombeats.session import WombeatsSession
 
 app = Flask(__name__)
 
 app.secret_key = 'super secret key'.encode('utf-8')
-# Set this to True for testing but you probaly want it set to False in production.
-SHOW_DIALOG = True
 
-
-def is_logged_in():
-    session['token_info'], authorized = get_token(session)
-    return 'token_info' in session
+wombeats_session = WombeatsSession(session)
 
 
 @app.after_request
@@ -30,40 +25,70 @@ def after_request(response):
     return response
 
 
-# authorization-code-flow Step 1. Have your application request authorization;
-# the user logs in and authorizes access
-@app.route("/login")
-def verify():
-    return '<h1>Welcome</h1>Please click <a href="/login">here</a> to log in.'
-
+# Browser Endpoints
 
 @app.route("/")
 def login():
-    # Don't reuse a SpotifyOAuth object because they store token info and you could leak user tokens if you reuse a SpotifyOAuth object
-    sp_oauth = spotipy.oauth2.SpotifyOAuth(client_id=CLI_ID, client_secret=CLI_SEC, redirect_uri=REDIRECT_URI,
-                                           scope=SCOPE)
+    # Checks spotify to see if the user is logged in
+    # After successful login verification from spotify,
+    # User is redirected to /api_callback (REDIRECT_URI)
+    sp_oauth = spotipy.oauth2.SpotifyOAuth(
+        client_id=CLI_ID,
+        client_secret=CLI_SEC,
+        redirect_uri=REDIRECT_URI,
+        scope=SCOPE
+    )
     auth_url = sp_oauth.get_authorize_url()
-    print(auth_url)
     return redirect(auth_url)
 
 
 @app.route("/index")
 def index():
-    session['token_info'], authorized = get_token(session)
-    if is_logged_in():
+    if wombeats_session.is_logged_in():
+        # This is the JS App entry point
         return redirect("/static/index.html")
 
+    # If we can't verify the user is logged in the session, redirect back to '/' flow
     return redirect("/")
 
 
+# 3rd party endpoints (Spotify)
+
+@app.route("/api_callback")
+def api_callback():
+    # Spotify calls this API endpoint after successful login validation
+    # Create auth object with developer API keys
+    sp_oauth = spotipy.oauth2.SpotifyOAuth(
+        client_id=CLI_ID,
+        client_secret=CLI_SEC,
+        redirect_uri=REDIRECT_URI,
+        scope=SCOPE
+    )
+
+    # This arg comes from spotify
+    code = request.args.get('code')
+
+    # Token = User code + sp_oauth (Developer token)
+    token_info = sp_oauth.get_access_token(code)
+
+    # Saving the access token into the user's cookies along with all other token related info
+    wombeats_session.set_token_session(token_info)
+
+    print('Session, auth_token, token info is: ', session, session['token_info'])
+
+    return redirect("index")
+
+
+# API Endpoints called by Javascript code
+
 @app.route("/search")
 def search():
-    if not is_logged_in():
+    if not wombeats_session.is_logged_in():
         return redirect('/')
 
     auth_manager = SpotifyClientCredentials()
     sp = spotipy.Spotify(auth_manager=auth_manager)
-    print('***session', session)
+
     api_access = SpotifyAPIAccess.build(client=sp)
     search_query = SearchQuery(
         artist=request.args.get('artist'),
@@ -80,49 +105,3 @@ def search():
     results = json.dumps([result.dict() for result in sorted_search_results])
     print(results)
     return results
-
-
-# authorization-code-flow Step 2.
-# Have your application request refresh and access tokens;
-# Spotify returns access and refresh tokens
-@app.route("/api_callback")
-def api_callback():
-    # Don't reuse a SpotifyOAuth object because they store token info and you could leak user tokens if you reuse a SpotifyOAuth object
-    sp_oauth = spotipy.oauth2.SpotifyOAuth(client_id=CLI_ID, client_secret=CLI_SEC, redirect_uri=REDIRECT_URI,
-                                           scope=SCOPE)
-    auth_manager = SpotifyClientCredentials()
-    auth_token = auth_manager.get_access_token()
-    session.clear()
-    code = request.args.get('code')
-    token_info = sp_oauth.get_access_token(code)
-
-    # Saving the access token along with all other token related info
-    session["token_info"] = token_info
-    print('Session, auth_token, token info is: ', session, auth_token, session['token_info'])
-
-    return redirect("index")
-
-
-# Checks to see if token is valid and gets a new token if not
-def get_token(session):
-    token_valid = False
-    token_info = session.get("token_info", {})
-
-    # Checking if the session already has a token stored
-    if not (session.get('token_info', False)):
-        token_valid = False
-        return token_info, token_valid
-
-    # Checking if token has expired
-    now = int(time.time())
-    is_token_expired = session.get('token_info').get('expires_at') - now < 60
-
-    # Refreshing token if it has expired
-    if (is_token_expired):
-        # Don't reuse a SpotifyOAuth object because they store token info and you could leak user tokens if you reuse a SpotifyOAuth object
-        sp_oauth = spotipy.oauth2.SpotifyOAuth(client_id=CLI_ID, client_secret=CLI_SEC, redirect_uri=REDIRECT_URI,
-                                               scope=SCOPE)
-        token_info = sp_oauth.refresh_access_token(session.get('token_info').get('refresh_token'))
-
-    token_valid = True
-    return token_info, token_valid
---
+++ wombeats/session.py
@@ -0,0 +1,43 @@
+import time
+
+import spotipy
+from flask import session
+
+from wombeats.constants import CLI_ID, CLI_SEC, REDIRECT_URI, SCOPE
+
+
+class WombeatsSession:
+    def __init__(self, session: session):
+        self.session = session
+
+    def is_logged_in(self):
+        session['token_info'], authorized = self.get_token()
+        return 'token_info' in session
+
+    def set_token_info(self, token_info):
+        self.session["token_info"] = token_info
+
+    def get_token(self):
+        token_info = self.session.get("token_info", {})
+
+        # Checking if the session already has a token stored
+        if not (self.session.get('token_info', False)):
+            token_valid = False
+            return token_info, token_valid
+
+        # Checking if token has expired
+        now = int(time.time())
+        is_token_expired = self.session.get('token_info').get('expires_at') - now < 60
+
+        # Refreshing token if it has expired
+        if is_token_expired:
+            sp_oauth = spotipy.oauth2.SpotifyOAuth(
+                client_id=CLI_ID,
+                client_secret=CLI_SEC,
+                redirect_uri=REDIRECT_URI,
+                scope=SCOPE
+            )
+
+            token_info = sp_oauth.refresh_access_token(self.session.get('token_info').get('refresh_token'))
+
+        return token_info
