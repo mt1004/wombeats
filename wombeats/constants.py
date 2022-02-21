e9cba48b6ac3f9897693a28b92815e7b691e4ab5
Nikhil Prasanna <nikprasanna@gmail.com>
2022-02-21 12:36:47 -0500

    User add profile info debugging

--- wombeats/constants.py
+++ wombeats/constants.py
@@ -3,5 +3,5 @@ CLI_ID = '09f8a8b99b0d4f1c804753c3c5451f09'
 CLI_SEC = '060ad82b313f4864b748c80f550a5cbc'
 # TODO: This should be from an environment variable
 REDIRECT_URI = "https://np-song-matcher-bpm.herokuapp.com/api_callback"
-SCOPE = 'playlist-modify-private,playlist-modify-public,user-top-read'
+SCOPE = 'playlist-modify-private,playlist-modify-public,user-top-read,user-read-private'
 SHOW_DIALOG = True
