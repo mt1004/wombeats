0615d2253374122ad602620e0ab96497ed5ba98d
Nikhil Prasanna <nikprasanna@gmail.com>
2022-06-25 17:39:49 -0400

    Refresh static files

--- wombeats-client/src/SearchInputForm.js
+++ wombeats-client/src/SearchInputForm.js
@@ -11,58 +11,131 @@ export function SearchInputForm(props) {
         <div>
             <Box
               component="form"
-              sx={{
-                '& > :not(style)': { m: 1, width: '25ch' },
-              }}
               noValidate
               autoComplete="off"
+                style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    width: "90%",
+                    height: "90%",
+                    padding: "10px",
+                    margin: "10px",
+                    backgroundColor: "lightblue",
+                    borderRadius: "10px",
+                    boxShadow: "0px 0px 10px 0px rgba(0,0,0,0.75)",
+                    border: "1px solid #ccc",
+                    flex: "1",
+                }}
             >
                 <form>
                     <TextField
-                      id="outlined-name"
+                      id="standard-basic"
                       label="Artist"
                       name="artist"
                       defaultValue={artist}
+                      variant="standard"
+                      style={{
+                          boxShadow: "0px 0px 10px 0px rgba(0,0,0,0.75)",
+                          backgroundColor: "white",
+                      }}
                     />
+                    &nbsp;&nbsp;&nbsp;&nbsp;
                     <TextField
-                      id="outlined-name"
+                      id="standard-basic"
                       label="Album"
                       name="album"
                       defaultValue={album}
+                      variant="standard"
+                      style={{
+                          boxShadow: "0px 0px 5px 0px rgba(0,0,0,0.75)",
+                          backgroundColor: "white",
+                      }}
                     />
+                    &nbsp;&nbsp;&nbsp;&nbsp;
                     <TextField
                       id="outlined-name"
                       label="Track"
                       name="track"
                       defaultValue={track}
+                      variant="standard"
+                      style={{
+                          boxShadow: "0px 0px 10px 0px rgba(0,0,0,0.75)",
+                          backgroundColor: "white",
+                      }}
                     />
+                    &nbsp;&nbsp;&nbsp;&nbsp;
                     <TextField
                       id="outlined-name"
                       label="Year"
                       name="year"
                       defaultValue={year}
+                      variant="standard"
+                      style={{
+                          boxShadow: "0px 0px 10px 0px rgba(0,0,0,0.75)",
+                          backgroundColor: "white",
+                      }}
                     />
+                    &nbsp;&nbsp;&nbsp;&nbsp;
                     <TextField
                       id="outlined-name"
                       label="Genre"
                       name="genre"
                       defaultValue={genre}
+                      variant="standard"
+                      style={{
+                          boxShadow: "0px 0px 10px 0px rgba(0,0,0,0.75)",
+                          backgroundColor: "white",
+                      }}
                     />
+                    &nbsp;&nbsp;&nbsp;&nbsp;
                     <TextField
                       id="outlined-name"
                       label="From BPM"
                       name="fromBpm"
                       defaultValue={fromBpm}
+                      variant="standard"
+                      style={{
+                          boxShadow: "0px 0px 10px 0px rgba(0,0,0,0.75)",
+                          backgroundColor: "white",
+                      }}
                     />
+                    &nbsp;&nbsp;&nbsp;&nbsp;
                     <TextField
                       id="outlined-name"
                       label="To BPM"
                       name="toBpm"
                       defaultValue={toBpm}
+                      variant="standard"
+                      style={{
+                          boxShadow: "0px 0px 10px 0px rgba(0,0,0,0.75)",
+                          backgroundColor: "white",
+                      }}
                     />
-                    <FormControlLabel control={<Checkbox defaultChecked={newMusicFriday} />} label="Search New Music Friday" name="newMusicFriday"/>
-                    <Button type="submit" variant="contained">Search</Button>
-                    <Button href="/" variant="contained">Reset</Button>
+                    <br/><br/>
+                    &nbsp;&nbsp;
+                    <FormControlLabel
+                        control={<Checkbox defaultChecked={newMusicFriday} />}
+                        label="New Music Friday"
+                        name="newMusicFriday"
+                    />
+                    <br/><br/>
+                    <Button
+                        type="submit"
+                        variant="contained"
+                        style={{
+                            backgroundColor: "white",
+                        }}
+                    >Search</Button>
+                    &nbsp;&nbsp;&nbsp;&nbsp;
+                    <Button
+                        href="/"
+                        variant="contained"
+                        style={{
+                            backgroundColor: "white",
+                        }}
+                    >Reset</Button>
                 </form>
             </Box>
         </div>
--- wombeats-client/src/SearchResultTable.js
+++ wombeats-client/src/SearchResultTable.js
@@ -11,27 +11,28 @@ export default function SearchResultTable(props) {
         { title: "Artist", field: "artist" },
         { title: "Album", field: "album" },
         { title: "Track", field: "track" },
-        { title: "Release Date", field: "release_date" },
         { title: "BPM", field: "bpm" },
-        {
-            title: "Browser",
-            field: "external_url",
-            render: rowData => <a href={rowData.external_url}>Open in browser</a>
-        },
         {
             title: "Spotify",
             field: "track_uri",
             render: rowData => <a href={rowData.track_uri}>Open in Spotify</a>
+        },
+        { title: "Release Date", field: "release_date" },
+        {
+            title: "Browser",
+            field: "external_url",
+            render: rowData => <a href={rowData.external_url}>Open in browser</a>
         }
+
     ]
 
-    useEffect(() => {
-    fetch("https://np-song-matcher-bpm.herokuapp.com/search?isLoading=true&artist=" + artist + "&album=" + album + "&track=" + track + "&year=" + year + "&genre=" + genre + "&fromBpm=" + fromBpm  + "&toBpm=" + toBpm + "&newMusicFriday=" + newMusicFriday)
-        .then(resp => resp.json())
-        .then(resp => {
-        setData(resp)
-        })
-    }, [])
+//    useEffect(() => {
+//    fetch("https://np-song-matcher-bpm.herokuapp.com/search?isLoading=true&artist=" + artist + "&album=" + album + "&track=" + track + "&year=" + year + "&genre=" + genre + "&fromBpm=" + fromBpm  + "&toBpm=" + toBpm + "&newMusicFriday=" + newMusicFriday)
+//        .then(resp => resp.json())
+//        .then(resp => {
+//        setData(resp)
+//        })
+//    }, [])
     return (
         <MaterialTable
             title="Song Data"
