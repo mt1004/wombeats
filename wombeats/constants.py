b4ef8f86c213f0c9af963a21b6dfefa54fb803b4
Michelle Tsai <min.chieh.tsai@gmail.com>
2023-08-17 16:26:48 -0400

    Update constants.py

--- wombeats/constants.py
+++ wombeats/constants.py
@@ -1,6 +1,6 @@
 API_BASE = 'https://accounts.spotify.com'
-CLI_ID = '09f8a8b99b0d4f1c804753c3c5451f09'
-CLI_SEC = '060ad82b313f4864b748c80f550a5cbc'
+CLI_ID = '[INPUT YOUR CLIENT ID]'
+CLI_SEC = '[INPUT YOUR SECRET]'
 # TODO: This should be from an environment variable
 REDIRECT_URI = "https://spotify-bpm-viewer.fly.dev/api_callback"
 SCOPE = 'playlist-modify-public,playlist-read-private,user-top-read,user-read-private'
